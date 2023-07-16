d7660353748fc6e530e6af68f5dc1005e3e41ec7
Rysias <jhvithamar@gmail.com>
2023-07-16 16:07:01 +0200

    add from_model method

--- src/batchata/batch.py
+++ src/batchata/batch.py
@@ -4,13 +4,25 @@ from .count import count_tokens
 
 
 class Batch:
-    def __init__(self, rate_limit: int = 1, token_limit: int | None = None):
+    def __init__(
+        self,
+        rate_limit: int = 1,
+        token_limit: int | None = None,
+        concurrent_size: int | None = None,
+    ):
         """Batch that can account for both rate_limit and token_limit"""
         self.rate_limit = rate_limit
         self.token_limit = token_limit if token_limit is not None else 100_000_000
+        self.concurrent_size = concurrent_size
+        self.batch = (
+            self.batch_concurrent if concurrent_size is not None else self.batch_rate
+        )
         pass
 
-    def batch(self, text_to_batch: Iterable) -> Generator[Iterable[str], None, None]:
+    def batch_rate(
+        self,
+        text_to_batch: Iterable,
+    ) -> Generator[Iterable[str], None, None]:
         """Batch while accounting for both rate_limit and token_limit"""
         current_tokens = 0
         rate_count = 0
@@ -32,3 +44,32 @@ class Batch:
 
         if batch:
             yield batch
+
+    def batch_concurrent(
+        self,
+        text_to_batch: Iterable,
+    ) -> Generator[Iterable[str], None, None]:
+        batch_size = (
+            self.concurrent_size
+            if self.concurrent_size is not None
+            else self.rate_limit
+        )
+        batch = []
+        for text in text_to_batch:
+            batch.append(text)
+            if len(batch) == batch_size:
+                yield batch
+                batch = []
+        if batch:
+            yield batch
+
+    @classmethod
+    def from_model(cls: "Batch", model_name: str) -> "Batch":
+        """Create a Batch from a model name"""
+        if model_name == "gpt-3.5-turbo":
+            return cls(rate_limit=3500, token_limit=90_000, concurrent_size=None)
+        if model_name == "gpt-4":
+            return cls(rate_limit=200, token_limit=40_000, concurrent_size=None)
+        if model_name.startswith("claude"):
+            return cls(concurrent_size=4)
+        raise ValueError(f"Unknown model name: {model_name}")
--- tests/test_batch.py
+++ tests/test_batch.py
@@ -1,30 +1,73 @@
+import pytest
 from batchata import Batch
+
 
 def test_simple_batch():
     text_to_batch = ["This is a test", "This is another test", "This is a third test"]
     batcher = Batch(rate_limit=1)
-    batches = [batch for batch in batcher.batch(text_to_batch)]
+    batches = list(batcher.batch(text_to_batch))
     assert len(batches) == 3
     assert batches[0] == ["This is a test"]
+
 
 def test_double_rate_limit():
     text_to_batch = ["This is a test", "This is another test", "This is a third test"]
     batcher = Batch(rate_limit=2)
-    batches = [batch for batch in batcher.batch(text_to_batch)]
+    batches = list(batcher.batch(text_to_batch))
     assert len(batches) == 2
     assert batches[0] == ["This is a test", "This is another test"]
 
+
 def test_token_limit():
-    text_to_batch = ["short word here", "short word here", "short word here", "a very long sentence with many words"]
+    text_to_batch = [
+        "short word here",
+        "short word here",
+        "short word here",
+        "a very long sentence with many words",
+    ]
     batcher = Batch(rate_limit=10, token_limit=9)
-    batches = [batch for batch in batcher.batch(text_to_batch)]
+    batches = list(batcher.batch(text_to_batch))
     assert len(batches) == 2
     assert batches[1] == ["a very long sentence with many words"]
 
+
 def test_mix_limts():
-    text_to_batch = ["short word here", "short word here", "short word here", "a very long sentence with many words"]
+    text_to_batch = [
+        "short word here",
+        "short word here",
+        "short word here",
+        "a very long sentence with many words",
+    ]
     batcher = Batch(rate_limit=1, token_limit=9)
-    batches = [batch for batch in batcher.batch(text_to_batch)]
+    batches = list(batcher.batch(text_to_batch))
     assert len(batches) == 4
     assert len(batches[0]) == 1
     assert batches[1] == ["short word here"]
+
+
+def test_concurrent():
+    text_to_batch = ["This is a test", "This is another test", "This is a third test"]
+    batcher = Batch(rate_limit=1, concurrent_size=2)
+    batches = list(batcher.batch_concurrent(text_to_batch))
+    assert len(batches) == 2
+    assert len(batches[0]) == 2
+    assert batches[1] == ["This is a third test"]
+
+
+def test_from_gpt():
+    batcher = Batch.from_model("gpt-3.5-turbo")
+    assert batcher.rate_limit == 3500
+    assert batcher.token_limit == 90_000
+
+
+def test_from_unknown():
+    with pytest.raises(ValueError, match=r"Unknown model name: .*"):
+        Batch.from_model("unknown-model")
+
+
+def test_from_claude():
+    to_batch = ["a", "b", "c", "d", "e", "f", "g", "h"]
+    batcher = Batch.from_model("claude")
+    batches = list(batcher.batch(to_batch))
+    assert batcher.concurrent_size == 4
+    assert len(batches) == 2
