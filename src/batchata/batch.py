7b3123156f942d9f3c70561f324e3f587fe2e1c7
Rysias <jhvithamar@gmail.com>
2023-07-16 15:36:35 +0200

    refactor and simplify

--- src/batchata/batch.py
+++ src/batchata/batch.py
@@ -1,38 +1,34 @@
-from typing import Sequence, Generator
+from collections.abc import Generator, Iterable
 
 from .count import count_tokens
 
+
 class Batch:
     def __init__(self, rate_limit: int = 1, token_limit: int | None = None):
+        """Batch that can account for both rate_limit and token_limit"""
         self.rate_limit = rate_limit
         self.token_limit = token_limit if token_limit is not None else 100_000_000
         pass
 
-
-    def batch(self, text_to_batch) -> Generator[Sequence[str], None, None]:
+    def batch(self, text_to_batch: Iterable) -> Generator[Iterable[str], None, None]:
         """Batch while accounting for both rate_limit and token_limit"""
         current_tokens = 0
         rate_count = 0
         batch = []
+
         for text in text_to_batch:
-            if len(batch) == 0:
-                batch.append(text)
-                current_tokens = count_tokens(text)
-                rate_count = 1
-            elif current_tokens + count_tokens(text) > self.token_limit:
-                yield batch
+            tokens = count_tokens(text)
+            token_check = current_tokens + tokens > self.token_limit
+            rate_check = rate_count % self.rate_limit == 0
+            if len(batch) == 0 or token_check or rate_check:
+                if batch:
+                    yield batch
                 batch = [text]
-                current_tokens = count_tokens(text)
-                rate_count = 1
-            elif rate_count == self.rate_limit:
-                yield batch
-                batch = [text]
-                current_tokens = count_tokens(text)
-                rate_count = 1
+                current_tokens = tokens
             else:
                 batch.append(text)
-                current_tokens += count_tokens(text)
-                rate_count += 1
+                current_tokens += tokens
+            rate_count += 1
+
         if batch:
             yield batch
-
